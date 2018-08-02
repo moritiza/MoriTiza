82b96af3312a8ea4e249881f856b1026a99d3333
Morteza Nasiri <n.mortezapc@gmail.com>
2018-08-02 16:36:12 +0430

    Revert "Push all files and folers"
    This reverts commit 6aa6488c687f2a11665934f08e9646d24b416189.

--- core/Config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('BASEPATH') or define('BASEPATH', str_replace('\\', '/', dirname(dirname(dirname(__FILE__))) . '/'));
-defined('VIEWPATH') or define('VIEWPATH', BASEPATH . 'views/');
-defined('SESSIONPATH') or define('SESSIONPATH', BASEPATH . 'core/storage/sessions');
